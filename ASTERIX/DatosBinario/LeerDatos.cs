b93be5964e4968d5986e853903fa3aafe83b3bea
Adrian Escribano <adrianescribano2005@gmail.com>
2026-09-23 20:57:17 +0200

    mvnfb

--- ASTERIX/DatosBinario/LeerDatos.cs
+++ ASTERIX/DatosBinario/LeerDatos.cs
@@ -6,6 +6,7 @@ namespace Archvios
 {
     public class LeerDatos
     {
+        //nvjfdvbfdj
         public List<Mensaje> DatosProcesados(string nombreArchivo)
         {
             List<Mensaje> listaTiras = new List<Mensaje>();
