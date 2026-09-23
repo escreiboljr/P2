19cb1bd5c385678ddfff310d4203a4a07a40ede6
Adrian Escribano <adrianescribano2005@gmail.com>
2026-09-23 20:59:25 +0200

    Funcion sin OutOfRange

--- ASTERIX/DatosBinario/LeerDatos.cs
+++ ASTERIX/DatosBinario/LeerDatos.cs
@@ -6,7 +6,6 @@ namespace Archvios
 {
     public class LeerDatos
     {
-        //nvjfdvbfdj
         public List<Mensaje> DatosProcesados(string nombreArchivo)
         {
             List<Mensaje> listaTiras = new List<Mensaje>();
